6eb1b7eb47400bdee8de53bc4eb804cb918d0766
Raviraj <ravirajchilka10@gmail.com>
2024-06-06 16:43:46 -0400

    left side component changes

--- ReactJS/vite-project/src/App.jsx
+++ ReactJS/vite-project/src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "./Header"
-import Footer from "./Footer"
-
+import AddItem from './LeftSide/AddItem/AddItem'
 
 
 function App() {
@@ -13,8 +9,7 @@ function App() {
 
   return (
     <>
-      <Header></Header>
-      <Footer></Footer>
+   <AddItem></AddItem>
 
     </>
   )
